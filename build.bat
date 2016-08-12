284b3eff22c024ee06c936a1fe345953f4fd4687
Galgenjunge <galgenjunge@me.com>
2016-08-12 16:23:01 +0200

    1.0.8.4 - add node_modules, generalize build.bat

--- build.bat
+++ build.bat
@@ -1,1 +1,3 @@
-electron-packager ./app necrobotvisualizer "Pokemon Bot Visualizer" --platform=win32 --arch=x64 --version=1.3.3 --asar=true --overwrite --icon=C:\Users\AdrianGoebbels\Desktop\necrobotvisualizer-master\build\icon.ico --out=C:\Users\AdrianGoebbels\Desktop\pgo
+electron-packager ./app necrobotvisualizer "Pokemon Bot Visualizer" --platform=win32 --arch=x64 --version=1.3.3 --asar=true --overwrite --icon=./build/icon.ico --out=./build/
+
+pause
